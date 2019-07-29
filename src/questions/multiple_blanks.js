a17ba99400bcb0e221f509f9ba5631f48fa83018
Moises Baltazar <me@moisesb.com>
2019-07-29 14:19:03 -0500

    Create multiple blanks class

---
+++ src/questions/multiple_blanks.js
@@ -0,0 +1,20 @@
+import { Question, Answer } from './generic'
+
+export class FillInMultipleBlanks extends Question {
+    constructor(a) {
+        super(a)
+    }
+
+    static get canvas_type() {
+        return 'fill_in_multiple_blanks_question'
+    }
+
+    static get type() {
+        return 'Multiple Blanks'
+    }
+
+    static fromSimple(obj) {
+        const answers = Answer.mapAnswerGroups(obj.answers)
+        return new FillInMultipleBlanks({ ...obj, answers })
+    }
+}
