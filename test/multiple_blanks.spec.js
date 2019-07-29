df411514441bf302fd2372d30bfbfbaa2868126c
Moises Baltazar <me@moisesb.com>
2019-07-29 14:44:50 -0500

    Lint formatting of unit tests

--- test/multiple_blanks.spec.js
+++ test/multiple_blanks.spec.js
@@ -1,44 +1,39 @@
 import 'chai/register-should'
-import {describe} from "mocha"
-import {FillInMultipleBlanks} from "../src/questions/multiple_blanks"
+import { describe } from 'mocha'
+import { FillInMultipleBlanks } from '../src/questions/multiple_blanks'
 
 describe('Fill In Multiple Blanks', () => {
-	const simple_multiblanks = {
-		text: "<p>Roses are [color1], violets are [color2]</p>\n",
-		type: "Fill In Multiple Blanks",
-		answers: {
-			color1: [
-				"red",
-				"pink",
-				"white"
-			],
-			color2: [
-				"blue",
-				"violet"
-			]
-		}
-	}
-	const question = FillInMultipleBlanks.fromSimple(simple_multiblanks)
+    const simple_multiblanks = {
+        text: '<p>Roses are [color1], violets are [color2]</p>\n',
+        type: 'Fill In Multiple Blanks',
+        answers: {
+            color1: ['red', 'pink', 'white'],
+            color2: ['blue', 'violet'],
+        },
+    }
+    const question = FillInMultipleBlanks.fromSimple(simple_multiblanks)
 
-
-	it('is an instance of MultipleBlanks', () => {
-		question.should.be.an.instanceOf(FillInMultipleBlanks)
-	})
-	it('has correct prompt', () => {
-		question.should.have.property('text', '<p>Roses are [color1], violets are [color2]</p>')
-	})
-	it('has correct number of answer groups', () => {
-		Object.keys(question.answers).length.should.equal(2)
-	})
-	it('color1 has 3 answers', () => {
-		question.answers.should.have.property('color1')
-		question.answers.color1.length.should.equal(3)
-	})
-	it('color2 has 2 answers', () => {
-		question.answers.should.have.property('color2')
-		question.answers.color2.length.should.equal(2)
-	})
-	it('is worth 1 points', () => {
-		question.should.have.property('points', 1)
-	})
+    it('is an instance of MultipleBlanks', () => {
+        question.should.be.an.instanceOf(FillInMultipleBlanks)
+    })
+    it('has correct prompt', () => {
+        question.should.have.property(
+            'text',
+            '<p>Roses are [color1], violets are [color2]</p>',
+        )
+    })
+    it('has correct number of answer groups', () => {
+        Object.keys(question.answers).length.should.equal(2)
+    })
+    it('color1 has 3 answers', () => {
+        question.answers.should.have.property('color1')
+        question.answers.color1.length.should.equal(3)
+    })
+    it('color2 has 2 answers', () => {
+        question.answers.should.have.property('color2')
+        question.answers.color2.length.should.equal(2)
+    })
+    it('is worth 1 points', () => {
+        question.should.have.property('points', 1)
+    })
 })
--- test/short_answer.spec.js
+++ test/short_answer.spec.js
@@ -1,41 +1,38 @@
 import 'chai/register-should'
-import {describe} from "mocha"
-import {ShortAnswer} from "../src/questions/short_answer"
+import { describe } from 'mocha'
+import { ShortAnswer } from '../src/questions/short_answer'
 
 describe('Short Answer', () => {
-	const simple_shortanswer = {
-		text: "What is one of the first two numbers?",
-		type: "ShortAnswer",
-		answers: [
-			1,
-			"One",
-			"~ 2",
-			"~    Two"
-		]
-	}
-	const question = ShortAnswer.fromSimple(simple_shortanswer)
+    const simple_shortanswer = {
+        text: 'What is one of the first two numbers?',
+        type: 'ShortAnswer',
+        answers: [1, 'One', '~ 2', '~    Two'],
+    }
+    const question = ShortAnswer.fromSimple(simple_shortanswer)
 
-	it('is correct instance', () => {
-		question.should.be.an.instanceOf(ShortAnswer)
-	})
-	it('has correct prompt', () => {
-		question.should.have.property('text', 'What is one of the first two numbers?')
-	})
-	it('has correct number of answers', () => {
-		question.answers.length.should.equal(4)
-	})
-	it('correct answers are identified', () => {
-		question.answers.forEach(ans => {
-			ans.isCorrect.should.be.true
-		})
-	})
-	it('wrong answers are identified', () => {
-		question.answers.forEach(ans => {
-			ans.isCorrect.should.not.be.false
-		})
-	})
-	it('is worth 1 points', () => {
-		question.should.have.property('points', 1)
-	})
-
+    it('is correct instance', () => {
+        question.should.be.an.instanceOf(ShortAnswer)
+    })
+    it('has correct prompt', () => {
+        question.should.have.property(
+            'text',
+            'What is one of the first two numbers?',
+        )
+    })
+    it('has correct number of answers', () => {
+        question.answers.length.should.equal(4)
+    })
+    it('correct answers are identified', () => {
+        question.answers.forEach(ans => {
+            ans.isCorrect.should.be.true
+        })
+    })
+    it('wrong answers are identified', () => {
+        question.answers.forEach(ans => {
+            ans.isCorrect.should.not.be.false
+        })
+    })
+    it('is worth 1 points', () => {
+        question.should.have.property('points', 1)
+    })
 })
