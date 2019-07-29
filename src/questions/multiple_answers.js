88a9b7306f000028b521cfb734fdd754518d0807
Moises Baltazar <me@moisesb.com>
2019-07-29 14:15:29 -0500

    Create multiple answers class

---
+++ src/questions/multiple_answers.js
@@ -0,0 +1,20 @@
+import { Question, Answer } from './generic'
+
+export class MultipleAnswers extends Question {
+    constructor(a) {
+        super(a)
+    }
+
+    static get canvas_type() {
+        return 'multiple_answers_question'
+    }
+
+    static get type() {
+        return 'Multiple Answers'
+    }
+
+    static fromSimple(obj) {
+        const answers = Answer.mapAnswers(obj.answers)
+        return new MultipleAnswers({ ...obj, answers })
+    }
+}
