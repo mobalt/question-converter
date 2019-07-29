39d57af2676f46cd832081c3685c3f7bbb52a7df
Moises Baltazar <me@moisesb.com>
2019-07-29 17:17:55 -0500

    Fix formatting

--- test/simple_questions/index.spec.js
+++ test/simple_questions/index.spec.js
@@ -4,12 +4,12 @@ import { types, fromSimple } from '../../src/questions'
 
 describe('#fromSimple', function() {
     it('should be an instance of Text', function() {
-		const q = fromSimple({ type: 'Text' })
+        const q = fromSimple({ type: 'Text' })
         q.should.be.instanceOf(types.Text)
     })
-	it('should be an instance of MC', function() {
-		const q = fromSimple({ type: 'Multiple Choice' })
-		q.should.be.instanceOf(types.MultipleChoice)
-	})
-	// todo: add more types
+    it('should be an instance of MC', function() {
+        const q = fromSimple({ type: 'Multiple Choice' })
+        q.should.be.instanceOf(types.MultipleChoice)
+    })
+    // todo: add more types
 })
