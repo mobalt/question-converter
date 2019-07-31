8e19fe73bd8f1472632e62cd932c88f56c17d5eb
Moises Baltazar <me@moisesb.com>
2019-07-30 21:50:14 -0500

    Pass easy unit tests for MultipleBlanks

--- test/canvas_questions/multiple_blanks.spec.js
+++ test/canvas_questions/multiple_blanks.spec.js
@@ -3,21 +3,21 @@ import { describe } from 'mocha'
 import canvas_questions from './questions'
 import { FillInMultipleBlanks as QuestionType } from '../../src/questions/multiple_blanks'
 
-describe.skip('Fill In Multiple Blanks', () => {
+describe('Fill In Multiple Blanks', () => {
     const canvas_question_obj = canvas_questions[3]
     const question = QuestionType.fromCanvas(canvas_question_obj)
 
-    it.skip('is an instance of MultipleBlanks', () => {
+    it('is an instance of MultipleBlanks', () => {
         question.should.be.an.instanceOf(QuestionType)
     })
-    it.skip('has correct prompt', () => {
+    it('has correct prompt', () => {
         question.should.have.property(
             'text',
-            '<p>Roses are [color1], violets are [color2]</p>',
+            '<p><span>Roses <strong>are</strong> [color1], violets <strong>are</strong> [color2]</span></p>',
         )
     })
-    it.skip('has correct number of answer groups', () => {
-        Object.keys(question.answers).length.should.equal(2)
+    it('has correct number of answer groups', () => {
+        Object.keys(question.answers).length.should.equal(3)
     })
     it.skip('color1 has 3 answers', () => {
         question.answers.should.have.property('color1')
@@ -27,7 +27,7 @@ describe.skip('Fill In Multiple Blanks', () => {
         question.answers.should.have.property('color2')
         question.answers.color2.length.should.equal(2)
     })
-    it.skip('is worth 1 points', () => {
+    it('is worth 1 points', () => {
         question.should.have.property('points', 1)
     })
 })
