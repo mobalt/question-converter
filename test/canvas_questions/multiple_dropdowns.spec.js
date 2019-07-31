0dd01798d0501e65f5fe394b227494cbdfdf76c3
Moises Baltazar <me@moisesb.com>
2019-07-30 22:03:30 -0500

    Add unit tests for MultipleDropdowns

---
+++ test/canvas_questions/multiple_dropdowns.spec.js
@@ -0,0 +1,41 @@
+import 'chai/register-should'
+import { describe } from 'mocha'
+import canvas_questions from './questions'
+import { MultipleDropdowns as QuestionType } from '../../src/questions/multiple_dropdowns'
+
+describe.skip('Multiple Dropdowns', () => {
+    const canvas_question_obj = canvas_questions[5]
+    const question = QuestionType.fromCanvas(canvas_question_obj)
+
+    it.skip('is an instance of MultipleDropdowns', () => {
+        question.should.be.an.instanceOf(QuestionType)
+    })
+    it.skip('has correct prompt', () => {
+        question.should.have.property('text', 'Roses = [d1], Violets = [d2]')
+    })
+    it.skip('has correct number of answer groups', () => {
+        Object.values(question.answers).length.should.equal(2)
+    })
+    it.skip('d1 is correct', () => {
+        const { d1 } = question.answers
+        d1.length.should.equal(3)
+
+        const [a, b, c] = d1
+        a.should.have.property('isCorrect', true)
+        b.isCorrect.should.be.false
+        c.isCorrect.should.be.false
+    })
+    it.skip('d2 is correct', () => {
+        const { d2 } = question.answers
+        d2.length.should.equal(4)
+
+        const [a, b, c, d] = d2
+        a.should.have.property('isCorrect', true)
+        b.isCorrect.should.be.false
+        c.isCorrect.should.be.false
+        d.isCorrect.should.be.false
+    })
+    it.skip('is worth 1 point', () => {
+        question.should.have.property('points', 1)
+    })
+})
