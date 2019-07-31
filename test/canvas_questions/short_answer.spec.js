585244c766216668b1e102c0c429a5987ed7bc63
Moises Baltazar <me@moisesb.com>
2019-07-30 21:38:42 -0500

    Pass all unit tests

--- test/canvas_questions/short_answer.spec.js
+++ test/canvas_questions/short_answer.spec.js
@@ -3,33 +3,30 @@ import { describe } from 'mocha'
 import canvas_questions from './questions'
 import { ShortAnswer as QuestionType } from '../../src/questions/short_answer'
 
-describe.skip('Short Answer', () => {
+describe('Short Answer', () => {
     const canvas_question_obj = canvas_questions[2]
     const question = QuestionType.fromCanvas(canvas_question_obj)
 
-    it.skip('is correct instance', () => {
+    it('is correct instance', () => {
         question.should.be.an.instanceOf(QuestionType)
     })
-    it.skip('has correct prompt', () => {
-        question.should.have.property(
-            'text',
-            'What is one of the first two numbers?',
-        )
+    it('has correct prompt', () => {
+        question.should.have.property('text', '<p>Roses are _____. </p>')
     })
-    it.skip('has correct number of answers', () => {
-        question.answers.length.should.equal(4)
+    it('has correct number of answers', () => {
+        question.answers.length.should.equal(2)
     })
-    it.skip('correct answers are identified', () => {
+    it('correct answers are identified', () => {
         question.answers.forEach(ans => {
             ans.isCorrect.should.be.true
         })
     })
-    it.skip('wrong answers are identified', () => {
+    it('wrong answers are identified', () => {
         question.answers.forEach(ans => {
             ans.isCorrect.should.not.be.false
         })
     })
-    it.skip('is worth 1 points', () => {
+    it('is worth 1 points', () => {
         question.should.have.property('points', 1)
     })
 })
