5a5af607382e34c359ac0932ac864845b48d3a45
Moises Baltazar <me@moisesb.com>
2019-07-30 22:01:47 -0500

    Pass all unit tests

--- test/canvas_questions/multiple_answers.spec.js
+++ test/canvas_questions/multiple_answers.spec.js
@@ -3,36 +3,36 @@ import { describe } from 'mocha'
 import canvas_questions from './questions'
 import { MultipleAnswers as QuestionType } from '../../src/questions/multiple_answers'
 
-describe.skip('Multiple Answers', () => {
+describe('Multiple Answers', () => {
     const canvas_question_obj = canvas_questions[4]
     const question = QuestionType.fromCanvas(canvas_question_obj)
 
-    it.skip('is an instance of MultipleAnswers', () => {
+    it('is an instance of MultipleAnswers', () => {
         question.should.be.an.instanceOf(QuestionType)
     })
-    it.skip('has correct prompt', () => {
+    it('has correct prompt', () => {
         question.should.have.property(
             'text',
-            '<p> Two or more? <i>(select all that apply)</i></p>',
+            '<p>Solve. <img class="equation_image" title="\\sqrt{4}=?" src="http://fake.instructure.com/equation_images/%255Csqrt%257B4%257D%253D%253F" alt="LaTeX: \\sqrt{4}=?" data-equation-content="\\sqrt{4}=?"></p>',
         )
     })
-    it.skip('has correct number of answers', () => {
+    it('has correct number of answers', () => {
         question.answers.length.should.equal(5)
     })
-    it.skip('correct answers are identified', () => {
+    it('correct answers are identified', () => {
         const [a, b, c, d, e] = question.answers
 
+        a.should.have.property('isCorrect', true)
         b.should.have.property('isCorrect', true)
-        d.should.have.property('isCorrect', true)
     })
-    it.skip('wrong answers are identified', () => {
+    it('wrong answers are identified', () => {
         const [a, b, c, d, e] = question.answers
 
-        a.should.have.property('isCorrect', false)
         c.should.have.property('isCorrect', false)
+        d.should.have.property('isCorrect', false)
         e.should.have.property('isCorrect', false)
     })
-    it.skip('is worth 2 points', () => {
-        question.should.have.property('points', 2)
+    it('is worth 4 points', () => {
+        question.should.have.property('points', 4)
     })
 })
