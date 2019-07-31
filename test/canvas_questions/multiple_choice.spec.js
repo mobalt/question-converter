68b37a735b7636996b997355239bdaf42594f709
Moises Baltazar <me@moisesb.com>
2019-07-30 21:24:46 -0500

    Test prompt for MultipleChoice

--- test/canvas_questions/multiple_choice.spec.js
+++ test/canvas_questions/multiple_choice.spec.js
@@ -10,4 +10,12 @@ describe('Multiple Choice', () => {
     it('is an instance of MultipleChoice', () => {
         question.should.be.an.instanceOf(QuestionType)
     })
+
+    it('has correct prompt', () => {
+        question.should.have.property(
+            'text',
+            '<p><strong>Bold</strong>, <em>Italics</em>, <span style="text-decoration: underline;">Underline</span>.</p>',
+        )
+    })
+
 })
