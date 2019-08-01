442e4b6472352749b13cd6ab9013ebe1ef8cb09d
Moises Baltazar <me@moisesb.com>
2019-08-01 10:28:27 -0500

    Create first test for #toCanvas

---
+++ test/to_canvas/multiple_choice.spec.js
@@ -0,0 +1,47 @@
+import 'chai/register-should'
+import { describe } from 'mocha'
+import qs from '../../src/questions'
+
+describe.only('Multiple Choice', () => {
+    describe('#toCanvas', () => {
+        const simple = {
+            id: 999,
+            text: '<p> Multiple Choice Text </p>',
+            points: 1,
+            name: 'MC Question 1',
+            type: 'Multiple Choice',
+            answers: ['~ Correct One', 'Wrong 1', '<b>Wrong 2</b>'],
+            correct_comments: '<b>Yay!</b>',
+            incorrect_comments: 'Nay!',
+        }
+
+        const question = qs.fromSimple(simple)
+        const canvasObj = qs.toCanvas(question)
+
+        //# reference_obj is derived by console.log:
+        //console.log(canvasObj)
+        const reference_obj = {
+            id: 999,
+            question_name: 'MC Question 1',
+            points_possible: 1,
+            question_text: '<p> Multiple Choice Text </p>',
+            correct_comments_html: '<b>Yay!</b>',
+            incorrect_comments: 'Nay!',
+            answers: [
+                { text: 'Correct One', weight: 100 },
+                { text: 'Wrong 1', weight: 0 },
+                { html: '<b>Wrong 2</b>', weight: 0 },
+            ],
+            question_type: 'multiple_choice_question',
+        }
+
+        describe('The canvas question object', () => {
+            it('is a normal javascript object', () => {
+                canvasObj.should.be.an.instanceOf(Object)
+            })
+        })
+
+
+        })
+    })
+})
