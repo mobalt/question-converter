8a5548dceb37b5e2647d632850e83a918b1c16ca
Moises Baltazar <me@moisesb.com>
2019-08-01 22:45:56 -0500

    Test #toCanvas for Text

--- test/canvas_questions/text.spec.js
+++ test/canvas_questions/text.spec.js
@@ -38,4 +38,52 @@ describe('Text', () => {
             fullQuestion.should.deep.equal(question)
         })
     })
+
+    describe('#toCanvas', () => {
+        const { Answer } = qs.internal,
+            { Text } = qs.types
+
+        const question = new Text({
+            text: 'Just text.',
+            name: 'Question',
+            points: 0,
+            answers: [],
+        })
+        const canvasObj = qs.toCanvas(question)
+
+        describe('The canvas question object', () => {
+            it('is a normal javascript object', () => {
+                canvasObj.should.be.an.instanceOf(Object)
+            })
+
+            it('has zero answers', () => {
+                canvasObj.answers.should.be.an('array').but.empty
+            })
+
+            it('has canvas specific question_type', () => {
+                canvasObj.question_type.should.equal('text_only_question')
+            })
+
+            it('has #oneToOne fields', () => {
+                canvasObj.should.include({
+                    question_text: 'Just text.',
+                    points_possible: 0,
+                    question_name: 'Question',
+                })
+            })
+
+            describe('has #textHtml fields', () => {
+                it('all fields are excluded', () => {
+                    canvasObj.should.not.have.any.keys(
+                        'correct_comments',
+                        'correct_comments_html',
+                        'incorrect_comments',
+                        'incorrect_comments_html',
+                        'neutral_comments',
+                        'neutral_comments_html',
+                    )
+                })
+            })
+        })
+    })
 })
