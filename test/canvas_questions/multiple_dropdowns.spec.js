6435b7a79df9c6834cebe2317ea46167eb40c908
Moises Baltazar <me@moisesb.com>
2019-08-01 19:07:01 -0500

    Update #fromCanvas for MultipleDropdowns

--- test/canvas_questions/multiple_dropdowns.spec.js
+++ test/canvas_questions/multiple_dropdowns.spec.js
@@ -1,49 +1,82 @@
 import 'chai/register-should'
 import { describe } from 'mocha'
-import canvas_questions from './questions'
-import { MultipleDropdowns as QuestionType } from '../../src/questions/multiple_dropdowns'
 import qs from '../../src/questions'
+import canvas_questions from './questions'
 
 describe('Multiple Dropdowns', () => {
-    const canvas_question_obj = canvas_questions[5]
-    const question = qs.fromCanvas(canvas_question_obj)
-
-    it('is an instance of MultipleDropdowns', () => {
-        question.should.be.an.instanceOf(QuestionType)
-    })
-    it('has correct prompt', () => {
-        question.text.should.equal(
+    const canvas_obj = {
+        id: 6,
+        question_name: 'Multiple Dropdowns',
+        points_possible: 1,
+        question_text:
             '<p><span>Roses are [color1], violets are [color2]</span></p>',
-        )
-    })
-    it('has correct number of total answers', () => {
-        question.answers.should.have.lengthOf(5)
-    })
-    it('has correct answer groups', () => {
-        question.answerObj.should.be
-            .an('object')
-            .that.has.all.keys('color1', 'color2')
-    })
-    it('color1 group is correct', () => {
-        const sublist = question.answerObj.color1
+        answers: [
+            { id: 334, blank_id: 'color1', text: 'red', weight: 100 },
+            { id: 7106, blank_id: 'color1', text: 'black', weight: 0 },
+            { id: 2559, blank_id: 'color1', text: 'brown', weight: 0 },
+            { id: 6714, blank_id: 'color2', text: 'blue', weight: 100 },
+            {
+                id: 731,
+                blank_id: 'color2',
+                text: 'purple (no html)',
+                weight: 0,
+            },
+        ],
+        question_type: 'multiple_dropdowns_question',
+    }
 
-        sublist.should.have.lengthOf(3)
+    describe('#fromCanvas', () => {
+        const question = qs.fromCanvas(canvas_obj)
 
-        const [a, b, c] = sublist
-        a.isCorrect.should.be.true
-        b.isCorrect.should.be.false
-        c.isCorrect.should.be.false
-    })
-    it('color2 group is correct', () => {
-        const sublist = question.answerObj.color2
+        it('is an instance of MultipleDropdowns', () => {
+            question.should.be.an.instanceOf(qs.types.MultipleDropdowns)
+        })
 
-        sublist.should.have.lengthOf(2)
+        it('has correct prompt', () => {
+            question.text.should.equal(
+                '<p><span>Roses are [color1], violets are [color2]</span></p>',
+            )
+        })
 
-        const [a, b] = sublist
-        a.isCorrect.should.be.true
-        b.isCorrect.should.be.false
-    })
-    it('is worth 1 point', () => {
-        question.points.should.equal(1)
+        it('has correct number of total answers', () => {
+            question.answers.should.have.lengthOf(5)
+        })
+
+        it('has correct answer groups', () => {
+            question.answerObj.should.be
+                .an('object')
+                .that.has.all.keys('color1', 'color2')
+        })
+
+        it('color1 group is correct', () => {
+            const sublist = question.answerObj.color1
+
+            sublist.should.have.lengthOf(3)
+
+            const [a, b, c] = sublist
+            a.isCorrect.should.be.true
+            b.isCorrect.should.be.false
+            c.isCorrect.should.be.false
+        })
+
+        it('color2 group is correct', () => {
+            const sublist = question.answerObj.color2
+
+            sublist.should.have.lengthOf(2)
+
+            const [a, b] = sublist
+            a.isCorrect.should.be.true
+            b.isCorrect.should.be.false
+        })
+
+        it('is worth 1 point', () => {
+            question.points.should.equal(1)
+        })
+
+        it('can handle the extra fields of a full canvas object', () => {
+            const canvas_obj_full_version = canvas_questions[5]
+            const fullQuestion = qs.fromCanvas(canvas_obj_full_version)
+            fullQuestion.should.deep.equal(question)
+        })
     })
 })
