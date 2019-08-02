cee2c8b73dfb2a352cf6cb96c660df047cc25982
Moises Baltazar <me@moisesb.com>
2019-08-01 22:43:56 -0500

    Test #toCanvas for MultipleBlanks

--- test/canvas_questions/multiple_blanks.spec.js
+++ test/canvas_questions/multiple_blanks.spec.js
@@ -77,4 +77,106 @@ describe('Fill In Multiple Blanks', () => {
             fullQuestion.should.deep.equal(question)
         })
     })
+
+    describe('#toCanvas', () => {
+        const { Answer } = qs.internal,
+            { FillInMultipleBlanks } = qs.types
+
+        const answers = [
+            new Answer({ text: 'red', isCorrect: true, group: 'color1' }),
+            new Answer({ text: 'pink', isCorrect: true, group: 'color1' }),
+            new Answer({ text: 'white', isCorrect: true, group: 'color1' }),
+            new Answer({ text: 'blue', isCorrect: true, group: 'color2' }),
+            new Answer({ text: 'violet', isCorrect: true, group: 'color2' }),
+        ]
+        const question = new FillInMultipleBlanks({
+            text: '<p>Roses are [color1], violets are [color2]</p>',
+            name: 'Question',
+            points: 1,
+            answers,
+        })
+        const canvasObj = qs.toCanvas(question)
+
+        describe('The canvas question object', () => {
+            it('is a normal javascript object', () => {
+                canvasObj.should.be.an.instanceOf(Object)
+            })
+
+            it('has 5 answers', () => {
+                canvasObj.answers.should.be.an('array').with.lengthOf(5)
+            })
+
+            it('has canvas specific question_type', () => {
+                canvasObj.question_type.should.equal(
+                    'fill_in_multiple_blanks_question',
+                )
+            })
+
+            it('has #oneToOne fields', () => {
+                canvasObj.should.include({
+                    question_text:
+                        '<p>Roses are [color1], violets are [color2]</p>',
+                    points_possible: 1,
+                    question_name: 'Question',
+                })
+            })
+
+            describe('has #textHtml fields', () => {
+                it('all fields are excluded', () => {
+                    canvasObj.should.not.have.any.keys(
+                        'correct_comments',
+                        'correct_comments_html',
+                        'incorrect_comments',
+                        'incorrect_comments_html',
+                        'neutral_comments',
+                        'neutral_comments_html',
+                    )
+                })
+            })
+        })
+
+        describe('The canvas answer objects', () => {
+            const [a, b, c, d, e] = canvasObj.answers
+
+            it('are basic javascript types', () => {
+                a.should.be.an.instanceOf(Object)
+                b.should.be.an.instanceOf(Object)
+                c.should.be.an.instanceOf(Object)
+                d.should.be.an.instanceOf(Object)
+                e.should.be.an.instanceOf(Object)
+            })
+
+            it('belong to correct groups', () => {
+                a.blank_id.should.equal('color1')
+                b.blank_id.should.equal('color1')
+                c.blank_id.should.equal('color1')
+                d.blank_id.should.equal('color2')
+                e.blank_id.should.equal('color2')
+            })
+
+            it('has all answers marked as correct (weight=100)', () => {
+                a.weight.should.equal(100)
+                b.weight.should.equal(100)
+                c.weight.should.equal(100)
+                d.weight.should.equal(100)
+                e.weight.should.equal(100)
+            })
+
+            it('include correct text fields', () => {
+                a.text.should.equal('red')
+                b.text.should.equal('pink')
+                c.text.should.equal('white')
+                d.text.should.equal('blue')
+                e.text.should.equal('violet')
+            })
+
+            it('exclude wrong text fields', () => {
+                a.should.not.include.keys('html')
+                b.should.not.include.keys('html')
+                c.should.not.include.keys('html')
+                d.should.not.include.keys('html')
+                e.should.not.include.keys('html')
+            })
+        })
+    })
 })
